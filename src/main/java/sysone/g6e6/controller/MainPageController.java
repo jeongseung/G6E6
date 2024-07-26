fe9d7412c9bb0578afe079c8fa99e130f74ccfaa
taeyeon <garong0721@gmail.com>
2024-07-26 17:41:06 +0900

    fix : Cotroller와 FXML 연결

--- src/main/java/sysone/g6e6/controller/MainPageController.java
+++ src/main/java/sysone/g6e6/controller/MainPageController.java
@@ -1,12 +1,14 @@
 package sysone.g6e6.controller;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
+import java.util.List;
+
+import javafx.event.ActionEvent;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+
 import javafx.scene.Group;
-import javafx.scene.Parent;
+
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import sysone.g6e6.util.FXUtil;
 
@@ -15,68 +17,68 @@ public class MainPageController {
 	@FXML
 	private Label welcomeLabel;
 	@FXML
-	private Button handelNotice1;
+	private Label noticeLabel1;
 	@FXML
-	private Button handelNotice2;
+	private Label noticeLabel2;
 	@FXML
-	private Button handelNotice3;
+	private Label noticeLabel3;
+	@FXML
+	private Button noticeButton1;
+	@FXML
+	private Button noticeButton2;
+	@FXML
+	private Button noticeButton3;
 	@FXML
 	private Group noticeGroup1;
 	@FXML
 	private Group noticeGroup2;
 	@FXML
 	private Group noticeGroup3;
-	@FXML
-	private Button handelGoToSelectionMode;
-	@FXML
-	private Button handleGoToQuizSubmit;
-	@FXML
-	private Button handleGoToMyPage;
-	@FXML
-	private Button handleGoToReviewNote;
-	@FXML
-	private Button handleGoToRanking;
 
 	private NoticeService noticeService;
 
 	public MainPageController() {
 		this.noticeService = new NoticeService();
+
 	}
 
-	public void initailize() {
+	public void initialize() {
 		List<Notice> latestNotices = noticeService.getLatestNotices(3);
 
 		if (latestNotices.size() > 0) {
-			handleNotice1.setText(latestNotices.get(0).getTitle());
-			handleNotice1.setVisible(true);
+			noticeButton1.setText(latestNotices.get(0).getTitle());
+			noticeButton1.setVisible(true);
 		}
 
 		if (latestNotices.size() > 1) {
-			handleNotice2.setText(latestNotices.get(1).getTitle());
-			handleNotice2.setVisible(true);
+			noticeButton2.setText(latestNotices.get(1).getTitle());
+			noticeButton2.setVisible(true);
 		}
 
 		if (latestNotices.size() > 2) {
-			handleNotice3.setText(latestNotices.get(2).getTitle());
-			handleNotice3.setVisible(true);
+			noticeButton3.setText(latestNotices.get(2).getTitle());
+			noticeButton3.setVisible(true);
 		}
 	}
 
-	private void handleNotice1(ActionEvent event) {
+	@FXML
+	public void handleNotice1(ActionEvent event) {
 		if (latestNotices.size > 0) {
 			noticeLabel1.setText(latestNotices.get(0).getContent());
 			noticeGroup1.setVisible(true);
 		}
 	}
 
-	private void handleNotice2(ActionEvent event) {
+	@FXML
+	public void handleNotice2(ActionEvent event) {
 		if (latestNotices.size > 1) {
 			noticeLabel2.setText(latestNotices.get(1).getContent());
 			noticeGroup2.setVisible(true);
 		}
 	}
 
-	private void handleNotice3(ActionEvent event) {
+	@FXML
+	public void handleNotice3(ActionEvent event) {
 		if (latestNotices.size > 2) {
 			noticeLabel3.setText(latestNotices.get(2).getContent());
 			noticeGroup3.setVisible(true);
@@ -89,31 +91,36 @@ public class MainPageController {
 	}
 
 	// 게임생성페이지로 이동
-	private void handleGoToSelectMode(ActionEvent event) {
+	@FXML
+	public void handleGoToSelectMode(ActionEvent event) {
 		FXUtil fx = FXUtil.getInstance();
 		fx.changeScene("게임생성Page");
 	}
 
 	// 문제 생성페이지로 이동
-	private void handleGoToQuizSubmit(ActionEvent event) {
+	@FXML
+	public void handleGoToQuizSubmit(ActionEvent event) {
 		FXUtil fx = FXUtil.getInstance();
 		fx.changeScene("문제생성Page");
 	}
 
 	// 마이페이지로 이동
-	private void handleGoToMyPage(ActionEvent event) {
+	@FXML
+	public void handleGoToMyPage(ActionEvent event) {
 		FXUtil fx = FXUtil.getInstance();
 		fx.changeScene("MyPage");
 	}
 
 	// 오답노트 페이지로 이동
-	private void handleGoToReviewNote(ActionEvent event) {
+	@FXML
+	public void handleGoToReviewNote(ActionEvent event) {
 		FXUtil fx = FXUtil.getInstance();
 		fx.changeScene("ReviewNotePage");
 	}
 
 	// 랭킹페이지로 이동
-	private void handleGoToRanking(ActionEvent event) {
+	@FXML
+	public void handleGoToRanking(ActionEvent event) {
 		FXUtil fx = FXUtil.getInstance();
 		fx.changeScene("RankingPage");
 	}
