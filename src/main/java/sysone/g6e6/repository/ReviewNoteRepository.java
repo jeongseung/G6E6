b5a1b066eccfaf5cc49d62f30496d51154a05482
cookooyas <cookooyas@gmail.com>
2024-08-01 20:36:29 +0900

    feat : ReviewNoteRepository 쿼리문 수정

--- src/main/java/sysone/g6e6/repository/ReviewNoteRepository.java
+++ src/main/java/sysone/g6e6/repository/ReviewNoteRepository.java
@@ -14,20 +14,20 @@ public class ReviewNoteRepository {
 
 	public List<Quiz> findAllQuizByUserId(int id) throws Exception {
 		List<Quiz> quizzes = new ArrayList<>();
-		String sql = "SELECT * " +
-			"FROM quizzes " +
-			"WHERE quiz_id IN ( " +
-			"    SELECT quiz_id " +
-			"    FROM mistakes " +
-			"    WHERE user_id = ? " +
-			") " +
-			"AND quiz_id NOT IN ( " +
-			"    SELECT quiz_id " +
-			"    FROM errorreports " +
-			")";
+		String sql = "select *"
+			+ "from quizzes"
+			+ "where quiz_id in (select quiz_id"
+			+ "                  from mistakes"
+			+ "                  where user_id=?)"
+			+ "and quiz_id not in (select quiz_id "
+			+ "                    from errorreports "
+			+ "                    where error_id in (select error_id "
+			+ "                                       from usererrorreports "
+			+ "                                       where user_id=?));";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 
 		pstmt.setInt(1, id);
+		pstmt.setInt(2, id);
 		ResultSet rs = pstmt.executeQuery();
 		while (rs.next()) {
 			quizzes.add(new Quiz(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4)));
