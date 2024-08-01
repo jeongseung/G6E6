2c23347f54e8e5ef5c1abf0fe48fd7c4355cc7ce
taeyeon <garong0721@gmail.com>
2024-08-01 14:21:44 +0900

    refactor: 데이터베이스 연결 설정을 properties 파일에서 로드하도록 변경

--- src/main/java/sysone/g6e6/MainApplication.java
+++ src/main/java/sysone/g6e6/MainApplication.java
@@ -1,22 +1,31 @@
 package sysone.g6e6;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import sysone.g6e6.util.DBUtil;
 import sysone.g6e6.util.FXUtil;
 
-import java.io.IOException;
-
 public class MainApplication extends Application {
 	@Override
 	public void start(Stage stage) throws IOException {
-		// FXUtil.init(stage,".fxml",".css");
+		FXUtil.init(stage, "LoginPage.fxml", "");
 	}
 
 	public static void main(String[] args) {
-		// DBUtil.init("DB_URL","DB_ID","DB_PW");
-		// launch();
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileInputStream("src/main/resources/sysone/g6e6/db.properties"));
+			String dbURL = prop.getProperty("dbURL");
+			String dbID = prop.getProperty("dbID");
+			String dbPW = prop.getProperty("dbPW");
+			DBUtil.init(dbURL, dbID, dbPW);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		launch();
 	}
 }
