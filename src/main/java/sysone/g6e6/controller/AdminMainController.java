e75fa5df2ce2746c1c0a4dbebd47ef9a4bed6bf8
taeyeon <garong0721@gmail.com>
2024-08-02 18:26:19 +0900

    feat : 공지 개수 증가

--- src/main/java/sysone/g6e6/controller/AdminMainController.java
+++ src/main/java/sysone/g6e6/controller/AdminMainController.java
@@ -23,11 +23,7 @@ public class AdminMainController {
 	@FXML
 	private Label noticeLabel;
 	@FXML
-	private Button noticeButton1;
-	@FXML
-	private Button noticeButton2;
-	@FXML
-	private Button noticeButton3;
+	private Button noticeButton1, noticeButton2, noticeButton3, noticeButton4, noticeButton5, noticeButton6, noticeButton7, noticeButton8;
 	@FXML
 	private Group noticeGroup;
 
@@ -44,7 +40,7 @@ public class AdminMainController {
 		welcomeLabel.setText("환영합니다 " + nickname + "님");
 
 		try {
-			latestNotifications = adminMainService.getLatestNotifications(3);
+			latestNotifications = adminMainService.getLatestNotifications(8);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -63,6 +59,31 @@ public class AdminMainController {
 		if (latestNotifications.size() > 2) {
 			noticeButton3.setText(latestNotifications.get(2).getTitle());
 			noticeButton3.setVisible(true);
+		}
+
+		if (latestNotifications.size() > 3) {
+			noticeButton4.setText(latestNotifications.get(3).getTitle());
+			noticeButton4.setVisible(true);
+		}
+
+		if (latestNotifications.size() > 4) {
+			noticeButton5.setText(latestNotifications.get(4).getTitle());
+			noticeButton5.setVisible(true);
+		}
+
+		if (latestNotifications.size() > 5) {
+			noticeButton6.setText(latestNotifications.get(5).getTitle());
+			noticeButton6.setVisible(true);
+		}
+
+		if (latestNotifications.size() > 6) {
+			noticeButton7.setText(latestNotifications.get(6).getTitle());
+			noticeButton7.setVisible(true);
+		}
+
+		if (latestNotifications.size() > 7) {
+			noticeButton8.setText(latestNotifications.get(7).getTitle());
+			noticeButton8.setVisible(true);
 		}
 	}
 
@@ -103,6 +124,46 @@ public class AdminMainController {
 	public void handleNotice3(ActionEvent event) {
 		if (latestNotifications.size() > 2) {
 			noticeLabel.setText(latestNotifications.get(2).getContent());
+			noticeGroup.setVisible(true);
+		}
+	}
+
+	@FXML
+	public void handleNotice4(ActionEvent event) {
+		if (latestNotifications.size() > 3) {
+			noticeLabel.setText(latestNotifications.get(3).getContent());
+			noticeGroup.setVisible(true);
+		}
+	}
+
+	@FXML
+	public void handleNotice5(ActionEvent event) {
+		if (latestNotifications.size() > 4) {
+			noticeLabel.setText(latestNotifications.get(4).getContent());
+			noticeGroup.setVisible(true);
+		}
+	}
+
+	@FXML
+	public void handleNotice6(ActionEvent event) {
+		if (latestNotifications.size() > 5) {
+			noticeLabel.setText(latestNotifications.get(5).getContent());
+			noticeGroup.setVisible(true);
+		}
+	}
+
+	@FXML
+	public void handleNotice7(ActionEvent event) {
+		if (latestNotifications.size() > 6) {
+			noticeLabel.setText(latestNotifications.get(6).getContent());
+			noticeGroup.setVisible(true);
+		}
+	}
+
+	@FXML
+	public void handleNotice8(ActionEvent event) {
+		if (latestNotifications.size() > 7) {
+			noticeLabel.setText(latestNotifications.get(7).getContent());
 			noticeGroup.setVisible(true);
 		}
 	}
